fed05adb6e1da10dfe3496a0f30f44e11ac4741c
mojahid-belaman <mojahidbelaman@gmail.com>
2023-08-17 10:57:02 +0100

    filter product has a property isFav assign true

--- src/containers/Favorites.js
+++ src/containers/Favorites.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useContext } from "react";
 
-import FavoriteItem from '../components/Favorites/FavoriteItem';
-import './Products.css';
+import FavoriteItem from "../components/Favorites/FavoriteItem";
+import "./Products.css";
+import { ProductContext } from "../context/product-provider";
 
-const Favorites = props => {
-  const favoriteProducts = useSelector(state =>
-    state.shop.products.filter(p => p.isFavorite)
+const Favorites = () => {
+  const favoriteProducts = useContext(ProductContext).products.filter(
+    (p) => p.isFavorite
   );
+  console.log(favoriteProducts);
   let content = <p className="placeholder">Got no favorites yet!</p>;
   if (favoriteProducts.length > 0) {
     content = (
       <ul className="products-list">
-        {favoriteProducts.map(prod => (
+        {favoriteProducts.map((prod) => (
           <FavoriteItem
             key={prod.id}
             id={prod.id}

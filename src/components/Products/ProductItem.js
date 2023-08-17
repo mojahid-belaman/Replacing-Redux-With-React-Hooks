0ed4aa6e80e4a63786f82bd70d3cf31d71da3790
mojahid-belaman <mojahidbelaman@gmail.com>
2023-08-17 10:57:46 +0100

    add an action to add favorite product

--- src/components/Products/ProductItem.js
+++ src/components/Products/ProductItem.js
@@ -1,23 +1,23 @@
-import React from 'react';
+import React, { useContext } from "react";
 
-import Card from '../UI/Card';
-import './ProductItem.css';
+import Card from "../UI/Card";
+import "./ProductItem.css";
+import { ProductContext } from "../../context/product-provider";
 
-const ProductItem = props => {
-
-  const toggleFavHandler = () => {
-  };
+const ProductItem = (props) => {
+  const toggleFav = useContext(ProductContext).toggleFav;
+  const toggleFavHandler = () => toggleFav(props.id);
 
   return (
-    <Card style={{ marginBottom: '1rem' }}>
+    <Card style={{ marginBottom: "1rem" }}>
       <div className="product-item">
-        <h2 className={props.isFav ? 'is-fav' : ''}>{props.title}</h2>
+        <h2 className={props.isFav ? "is-fav" : ""}>{props.title}</h2>
         <p>{props.description}</p>
         <button
-          className={!props.isFav ? 'button-outline' : ''}
+          className={!props.isFav ? "button-outline" : ""}
           onClick={toggleFavHandler}
         >
-          {props.isFav ? 'Un-Favorite' : 'Favorite'}
+          {props.isFav ? "Un-Favorite" : "Favorite"}
         </button>
       </div>
     </Card>
